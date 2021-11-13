f899aab66c06e539d8ac4e8cf2d25ed673003b58
konradBalon <konrad.balon@gmail.com>
2021-11-13 14:36:50 +0100

    static config, js added

--- config/settings.py
+++ config/settings.py
@@ -126,6 +126,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (BASE_DIR.joinpath('static'),)
+STATIC_ROOT = BASE_DIR.joinpath('staticfiles')
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
